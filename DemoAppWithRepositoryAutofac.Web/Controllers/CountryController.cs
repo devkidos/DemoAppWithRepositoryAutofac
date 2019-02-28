0840233c346e4805bf3f1558b536dcbf40a30814
DevKido <devkidos@gmail.com>
2019-02-28 10:19:19 +0530

    Added Common project

--- DemoAppWithRepositoryAutofac.Web/Controllers/CountryController.cs
+++ DemoAppWithRepositoryAutofac.Web/Controllers/CountryController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using DemoAppWithRepositoryAutofac.Common;
 using DemoAppWithRepositoryAutofac.ViewModel;
 using DemoAppWithRepositoryAutofac.ViewModel.Request;
 using Newtonsoft.Json;
@@ -13,29 +15,38 @@ using Newtonsoft.Json;
 namespace DemoAppWithRepositoryAutofac.Web.Controllers
 {
     public class CountryController : Controller
-    { 
+    {
+        private IHttpClientService httpClientService;
+         
+        public CountryController(IHttpClientService httpClientService)
+        {
+            this.httpClientService = httpClientService;
+        }
         public ActionResult Index()
         {
-            var data = SearchCountries("");
+            dynamic dynamicObject = new ExpandoObject();
+            dynamicObject.PageNumber = 0;
+            dynamicObject.PageSize = 10;
+            dynamicObject.Search = "";
+            var data = SearchCountries(dynamicObject);
+
             return View(data);
         }
 
-        private static List<VMCountry> SearchCountries(string name)
+        private List<VMCountry> SearchCountries(ExpandoObject dynamicObject)
         {
             string apiUrl = "http://localhost/DemoAppWithRepositoryAutofac.API/";
-            var input = new
-            {
-                PageNumber= 0,
-                PageSize= 10,
-                Search= ""
-            };
-            string inputJson = JsonConvert.SerializeObject(input);// (new JavaScriptSerializer()).Serialize(input);
-            WebClient client = new WebClient();
-            client.Headers["Content-type"] = "application/json";
-            client.Encoding = Encoding.UTF8;
-            string json = client.UploadString(apiUrl + "countries", inputJson);
-            List<VMCountry> countries = (new JavaScriptSerializer()).Deserialize<List<VMCountry>>(json);
-            return countries;
+
+
+            var rersult = httpClientService.Post<List<VMCountry>>(apiUrl, dynamicObject,  null);
+
+            //string inputJson = JsonConvert.SerializeObject(dynamicObject);// (new JavaScriptSerializer()).Serialize(input);
+            //WebClient client = new WebClient();
+            //client.Headers["Content-type"] = "application/json";
+            //client.Encoding = Encoding.UTF8;
+            //string json = client.UploadString(apiUrl + "countries", inputJson);
+            //List<VMCountry> countries = (new JavaScriptSerializer()).Deserialize<List<VMCountry>>(json);
+            return rersult;
         }
     }
 }
